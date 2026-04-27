68c7f2a6708f740321ed66ead010b2aa6a0e2de3
Leta-Kasahun <dbu1601306@dbu.edu.et>
2026-04-27 15:54:58 +0300

    folder stractures defined

---
+++ backend/app/Exceptions/Custom/ApiException.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Exceptions\Custom;
+
+class ApiException
+{
+    /**
+     * Create a new class instance.
+     */
+    public function __construct()
+    {
+        //
+    }
+}
