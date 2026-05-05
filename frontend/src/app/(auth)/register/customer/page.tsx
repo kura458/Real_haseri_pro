e1967ce6f026f7764f74b3463c046a6ef69f93f2
Getabalewkemaw <getabalewkemaw@gmail.com>
2026-05-05 15:26:27 +0300

    Feat:HAS-10 As a Customer or Technician, I want to create an account so that I can access the platform and use services.

--- frontend/src/app/(auth)/register/customer/page.tsx
+++ frontend/src/app/(auth)/register/customer/page.tsx
@@ -1,3 +1,4 @@
+// Task: HAS-10 As a Customer or Technician, I want to create an account so that I can access the platform and use services.
 import { redirect } from "next/navigation";
 
 export default function RegisterCustomerPage() {
