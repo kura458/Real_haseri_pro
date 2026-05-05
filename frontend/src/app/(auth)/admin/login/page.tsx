7902215ce9d925a42c4455c4d526806b073c2802
Kuzu-A <dbu1601470@dbu.edu.et>
2026-05-05 15:02:01 +0300

    HAS-36 : Implementation of Admin login page

--- frontend/src/app/(auth)/admin/login/page.tsx
+++ frontend/src/app/(auth)/admin/login/page.tsx
@@ -1,3 +1,4 @@
+// This is the admin login page for the Haseri platform. It provides a secure interface for administrators to log in and manage the platform's settings and user accounts.
 import { AdminLoginForm } from "@/src/features/auth/admin/components/AdminLoginForm";
 import { Metadata } from "next";
 export const metadata: Metadata = {
